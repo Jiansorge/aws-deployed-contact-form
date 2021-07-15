d414c2b6ad54c77662426433f72641096c31702b
[jiansorge] <[ji@nsorge.com]>
2021-07-15 14:57:59 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "aws-deployed-contact-form",
+  },
+  plugins: ["gatsby-plugin-theme-ui", "gatsby-plugin-react-helmet"],
+};
