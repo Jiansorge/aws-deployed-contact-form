aa76a9f0846b8fdb5d5dfdeae646a65f0b587dd7
[jiansorge] <[ji@nsorge.com]>
2021-07-16 01:14:54 -0700

    initial form via material ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 // styles
 const pageStyles = {
@@ -13,6 +16,12 @@ const headingStyles = {
 }
 const headingAccentStyles = {
   color: "#663399",
+}
+const formStyles= {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  height:'100%',
 }
 const paragraphStyles = {
   marginBottom: 48,
@@ -127,56 +136,66 @@ const links = [
 
 // markup
 const IndexPage = () => {
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [message, setMessage] = React.useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
       <h1 style={headingStyles}>
-        Congratulations
+        UI Recruitment Test
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
+        <span style={headingAccentStyles}>MK Decision</span>
       </h1>
       <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+        Feel free to drop us a message. We'd love to hear from you! 
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+      <form className='' 
+        style={formStyles} 
+        onSubmit={(e) => handleSubmit(e)}
+        noValidate autoComplete="off">
+        <TextField
+            id="name"
+            label="Name"
+            placeholder="Enter name..."
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            required
+            autoFocus
+          />
+          <TextField
+            id="email"
+            label="Email"
+            placeholder="Enter email..."
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            type='email'
+            required
+          />
+          <TextField
+            id="message"
+            label="Message"
+            placeholder="Leave us a message!"
+            variant="outlined"
+            multiline
+            rows={4}
+            fullWidth
+            value={message}
+            onChange={e=>setMessage(e.target.value)}
+            required
+          />
+          <Button
+            variant="contained"
+            Primary
+            type='submit'
           >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+            Submit
+          </Button> 
+       </form>
     </main>
   )
 }
